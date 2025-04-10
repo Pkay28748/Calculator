64400dce7b33eacbb107207b93a338bfc4184757
pkay28748 <pkay28748@gmail.com>
2025-04-10 10:58:57 -0400

    basic math functions created

---
+++ calculator.js
@@ -0,0 +1,20 @@
+// Creating basic math functions
+
+function add (a,b){
+    return a + b ;
+};
+
+function subtract(a,b){
+    return a - b ;
+};
+
+function multiply(a,b){
+    return a * b ;
+};
+
+function divide(a,b){
+    if(b === 0){
+        throw "Error"; // You can't divide a number by 0 
+    }
+
+};
